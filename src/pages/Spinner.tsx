1d9923622b3bb8aacaf2374c08d9aa1a49f532fa
benhur <benhurbenny@iclould.com>
2024-09-23 23:44:31 +0530

    some error removal

--- src/pages/Spinner.tsx
+++ src/pages/Spinner.tsx
@@ -1,14 +1,5 @@
-import Button from "@/components/buttonComponent";
-
 export const Spinner = () => {
   return (
-    // <div className="h-[100vh] w-full flex flex-col item-center px-7 py-7 gap-8">
-    //   <div className="flex md:w-full w-full justify-between gap-8">
-    //     <div className="skeleton sm:w-32 w-48 h-8"></div>
-    //     <div className="skeleton sm:w-32 w-48 h-8"></div>
-    //   </div>
-    //   <div className="w-full skeleton h-[75vh]"></div>
-    // </div>
     <div className="px-10 md:px-20 lg:px-40 pt-16 pb-16">
       <div className="h-3/4 flex flex-col gap-8">
         <div className="flex justify-between">
